3e92d75c40e944c8ba332a94734968cff634aaf6
Danidu kavishan <127879066+dananjaya2002@users.noreply.github.com>
2025-12-30 16:12:19 +0530

    change

--- server/src/app.js
+++ server/src/app.js
@@ -6,12 +6,14 @@ const path = require("path");
 const app = express();
 
 // CORS MUST be first - allow all origins
-app.use(cors({
-  origin: '*',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // Middleware
 app.use(express.json());
